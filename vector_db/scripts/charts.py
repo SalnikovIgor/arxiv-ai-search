686c2875231b2f507c2fecd29b04aa83a3b3c265
Selifanchik <selifanchik@mail.ru>
2023-12-05 01:59:34 +0300

    years chart

--- vector_db/scripts/charts.py
+++ vector_db/scripts/charts.py
@@ -6,17 +6,12 @@ from matplotlib.backends.backend_pdf import PdfPages
 month = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
 month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-# pdf = PdfPages('Figures.pdf')
+df = pd.read_csv('/home/user/projects/arxiv_abs_Nov_2023.csv')
 
-df = pd.read_csv('/home/user/projects/arxiv-ai-search/arxiv_abs_Nov_2023.csv')
 
-# for col in df.columns:
-#     print(col)
-
-# print(df.head())
-# my_pie = df.groupby(['_year'])['_title'].count()
-#print(df._year.value_counts())
-# plot = my_pie.plot(kind='pie', figsize=(10, 10))
+my_pie = df[df._year > 2013].groupby(['_year'])['_title'].count()
+plot = my_pie.plot(kind='pie', figsize=(10, 10))
+plt.savefig('all_years.png')
 # pdf.savefig()
 #print(df.groupby(df._year)._title.count())
 
@@ -32,12 +27,12 @@ df = pd.read_csv('/home/user/projects/arxiv-ai-search/arxiv_abs_Nov_2023.csv')
 
 
 
-test = ['Apr', 'Aug', 'Feb', 'Jan', 'Jul', 'Jun', 'Mar', 'May', 'Nov', 'Oct', 'Sep']
-print('list')
-print(sorted(test, key=lambda x: month_list.index(x)))
-print('dict')
-print(sorted(test, key=lambda x: month[x]))
+# test = ['Apr', 'Aug', 'Feb', 'Jan', 'Jul', 'Jun', 'Mar', 'May', 'Nov', 'Oct', 'Sep']
+# print('list')
+# print(sorted(test, key=lambda x: month_list.index(x)))
+# print('dict')
+# print(sorted(test, key=lambda x: month[x]))
 
 
-test = df[df._year == '1993'].sort_values(by=df._month, key=lambda x: month[x])
-print(test)
+# test = df[df._year == '1993'].sort_values(by=df._month, key=lambda x: month[x])
+# print(test)
